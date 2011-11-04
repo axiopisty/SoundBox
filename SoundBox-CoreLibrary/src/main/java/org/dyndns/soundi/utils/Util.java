b995d32ac57c2e547d11f518d4cf4d868c55649b
Oliver Zemann <oliver.zemann@gmail.com>
2011-11-04 14:36:18 +0000

    added Util.java for messaging and other methods that does not fit everywhere else in the namespace

---
+++ SoundBox-CoreLibrary/src/main/java/org/dyndns/soundi/utils/Util.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dyndns.soundi.utils;
+
+/**
+ *
+ * @author oli
+ */
+public final class Util {
+
+    public static enum Component {
+
+        CORE, BROWSER, PLAYER, EVENTADMIN, DOWNLOADER, DOWNLOADQUEUE, PLAYERQUEUE, UNSPECIFIED
+    }
+    
+    public static void sendMessage(Component comp, String message)
+    {
+        System.out.println("["+comp.toString()+"]: " + message);
+    }
+}

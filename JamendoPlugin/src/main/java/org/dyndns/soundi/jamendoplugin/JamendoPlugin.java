1d79bd33049cc98f8f944e7c74e80ab71dc4358f
Oliver Zemann <oliver.zemann@gmail.com>
2011-11-07 11:59:43 +0000

    the merge destroyed the soundbox/pom.xml... fixed it

--- JamendoPlugin/src/main/java/org/dyndns/soundi/jamendoplugin/JamendoPlugin.java
+++ JamendoPlugin/src/main/java/org/dyndns/soundi/jamendoplugin/JamendoPlugin.java
@@ -4,24 +4,26 @@
  */
 package org.dyndns.soundi.jamendoplugin;
 
+<<<<<<< .mine
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+=======
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
+>>>>>>> .r37
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-import org.dyndns.soundi.portals.interfaces.Author;
-import org.dyndns.soundi.portals.interfaces.CommunicationAction;
-import org.dyndns.soundi.portals.interfaces.IPortal;
-import org.dyndns.soundi.portals.interfaces.License;
-import org.dyndns.soundi.portals.interfaces.PluginInformation;
-import org.dyndns.soundi.portals.interfaces.Song;
+import org.dyndns.soundi.portals.interfaces.*;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -54,9 +56,25 @@ public class JamendoPlugin extends IPortal {
 
     @Override
     public Object searchSong(String searchString) {
-        String link = "http://api.jamendo.com/get2/id+name+album_name+artist_name+duration/track/json/track_album+album_artist?order=searchweight_desc&n=all&searchquery=" + searchString;
+
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+        } catch (Exception ex) {
+            System.out.println(ex.getLocalizedMessage());
+        }
+        // Prepare a request object
+        HttpGet httpget = new HttpGet("http://www.google.org/");
+
+       
+
+        URI link = null;
+        try {
+            link = new URI("http://api.jamendo.com/get2/id+name+album_name+artist_name+duration/track/json/track_album+album_artist?order=searchweight_desc&n=all&searchquery=" + searchString);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(JamendoPlugin.class.getName()).log(Level.SEVERE, null, ex);
+        }
         HttpGet get = new HttpGet(link);
-        DefaultHttpClient client = new DefaultHttpClient();
+        HttpClient client = new DefaultHttpClient();
         Dictionary l = new Hashtable<Object, Object>();
         ArrayList songArrayList = new ArrayList();
         JSONParser parser = new JSONParser();
@@ -64,24 +82,28 @@ public class JamendoPlugin extends IPortal {
         try {
             HttpEntity entity = client.execute(get).getEntity();
             String entityAsString = EntityUtils.toString(entity);
-            if(entityAsString.isEmpty())
-                // something went wrong...
+            entity.getContent().close();
+            if (entityAsString.isEmpty()) // something went wrong...
+            {
                 return null;
+            }
             obj = parser.parse(entityAsString);
+
         } catch (Exception ex) {
             Logger.getLogger(JamendoPlugin.class.getName()).log(Level.SEVERE, null, ex);
         }
+
         JSONArray songs = (JSONArray) obj;
         Iterator iter = songs.iterator();
         JSONObject rawSong = null;
         while (iter.hasNext()) {
             try {
-                 rawSong = (JSONObject) parser.parse(iter.next().toString());
+                rawSong = (JSONObject) parser.parse(iter.next().toString());
             } catch (Exception ex) {
                 Logger.getLogger(JamendoPlugin.class.getName()).log(Level.SEVERE, null, ex);
             }
-             
-            Song song = new JamendoSong(rawSong);
+
+            JamendoSong song = new JamendoSong(rawSong);
             songArrayList.add(song);
         }
 
@@ -110,9 +132,8 @@ public class JamendoPlugin extends IPortal {
 
     @Override
     public void handleEvent(Event event) {
-        
-        if(event.getTopic().equals(CommunicationAction.SEARCHSONGFORBROWSER.toString()))
-        {
+
+        if (event.getTopic().equals(CommunicationAction.SEARCHSONGFORBROWSER.toString())) {
             searchSong(event.getProperty("songTitle").toString());
         }
     }

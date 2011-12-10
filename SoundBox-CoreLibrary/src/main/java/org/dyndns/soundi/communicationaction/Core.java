aafc807a3bd875c563b1a472e992417016727a19
Oliver Zemann <oliver.zemann@gmail.com>
2011-12-10 16:28:57 +0100

    refactoring...

---
+++ SoundBox-CoreLibrary/src/main/java/org/dyndns/soundi/communicationaction/Core.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dyndns.soundi.communicationaction;
+
+/**
+ *
+ * @author oli
+ */
+
+
+enum Requests {
+
+    /**
+     * This is a request from the SoundBox Core to set the Browser UI visible.
+     */
+    SETBROWSERVISIBLE {
+
+        @Override
+        public String toString() {
+            return "org/dyndns/soundi/soundbox/SETBROWSERVISIBLE";
+        }
+    },
+    /**
+     * This is a request from the SoundBox Core to set the Browser UI invisible.
+     */
+    SETBROWSERINVISIBLE {
+
+        @Override
+        public String toString() {
+            return "org/dyndns/soundi/soundbox/SETBROWSERINVISIBLE";
+        }
+    },
+    /**
+     * This is a request from the SoundBox Core to set the Downloader UI visible.
+     */
+    SETDOWNLOADERVISIBLE {
+
+        @Override
+        public String toString() {
+            return "org/dyndns/soundi/soundbox/SETDOWNLOADERVISIBLE";
+        }
+    },
+    /**
+     * This is a request from the SoundBox Core to set the Downloader UI invisible.
+     */
+    SETDOWNLOADERINVISIBLE {
+
+        @Override
+        public String toString() {
+            return "org/dyndns/soundi/soundbox/SETDOWNLOADERINVISIBLE";
+        }
+    },
+    /**
+     * This is a request from the SoundBox Core to set the Player UI visible.
+     */
+    SETPLAYERVISIBLE {
+
+        @Override
+        public String toString() {
+            return "org/dyndns/soundi/soundbox/SETPLAYERVISIBLE";
+        }
+    },
+    /**
+     * This is a request from the SoundBox Core to set the Player UI invisible.
+     */
+    SETPLAYERINVISIBLE {
+
+        @Override
+        public String toString() {
+            return "org/dyndns/soundi/soundbox/SETPLAYERINVISIBLE";
+        }
+    },
+    /**
+     * This is a request from the PluginRegistry that a (new) portal is registered.
+     * (TODO: document the event api and its payloads).
+     */
+    REGISTERPORTAL {
+
+        @Override
+        public String toString() {
+            return "org/dyndns/soundi/soundbox/pluginregistry/REGISTERPORTAL";
+        }
+    },
+    /**
+     * This is a request from the PluginRegistry that a portal is unregistered.
+     * (TODO: document the event api and its payloads).
+     */
+    UNREGISTERPORTAL {
+
+        @Override
+        public String toString() {
+            return "org/dyndns/soundi/soundbox/pluginregistry/UNREGISTERPORTAL";
+        }
+    },
+
+}
---
+++ SoundBox-CoreLibrary/src/main/java/org/dyndns/soundi/communicationaction/PlayerEngine.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dyndns.soundi.communicationaction;
+
+/**
+ *
+ * @author oli
+ */
+public final class PlayerEngine {
+
+    private PlayerEngine() {
+    }
+
+    public static enum Requests {
+    }
+
+    public static enum Responses {
+    }
+}

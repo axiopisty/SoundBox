638bd1f557ae6d486eaa0f06e975accd03769127
Oliver Zemann <oliver.zemann@gmail.com>
2011-12-24 10:44:16 +0100

    further modifications in the git structure...

---
+++ src/main/java/org/dyndns/soundi/soundbox/Activator.java
@@ -0,0 +1,33 @@
+/**
+ * This is the default activator for the main application SoundBox OSGi.
+ *
+ * @since 1.0
+ */
+package org.dyndns.soundi.soundbox;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * This is the main class which must be the first one that gets called - as osgi
+ * bundle of course. This class provides the updater, the showGui call, and the
+ * exit function of the application.
+ *
+ * @version 0.0.1
+ * @author oli
+ */
+public class Activator implements BundleActivator {
+
+    @Override
+    public final void start(final BundleContext context) {
+
+        final SoundBox box = new SoundBox(context);
+        box.init();
+    }
+
+    @Override
+    public void stop(final BundleContext context) {
+        // maybe there should be an CommunicationAction.MAINAPPCLOSED message
+        // so that a nice "close" of the application can be made...
+    }
+}
